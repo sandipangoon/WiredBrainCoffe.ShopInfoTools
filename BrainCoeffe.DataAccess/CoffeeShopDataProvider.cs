e60da08b06e8384373937396f0dabd2d634a7657
Sandipan Goon <sandipan.goon@gmail.com>
2020-07-05 10:22:54 -0500

    Add Brain Coffee Data access

---
+++ BrainCoeffe.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using BrainCoeffe.DataAccess.Model;
+using System.Collections.Generic;
+
+namespace BrainCoeffe.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 120 };
+            yield return new CoffeeShop { Location = "London", BeansInStockInKg = 130 };
+            yield return new CoffeeShop { Location = "Paris", BeansInStockInKg = 210 };
+        }
+    }
+}
---
+++ BrainCoeffe.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,8 @@
+﻿namespace BrainCoeffe.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int BeansInStockInKg { get; set; }
+    }
+}
