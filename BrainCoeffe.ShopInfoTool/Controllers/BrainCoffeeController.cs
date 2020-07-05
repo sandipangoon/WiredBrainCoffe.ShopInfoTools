bce7f36e0fd4f67d1aa400d0aeeb2b27b08925b5
Sandipan Goon <sandipan.goon@gmail.com>
2020-07-05 10:23:44 -0500

    Integrated data access

---
+++ BrainCoeffe.ShopInfoTool/Controllers/BrainCoffeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BrainCoeffe.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrainCoeffe.ShopInfoTool.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrainCoffeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCoffeeShops()
+        {
+            var coffeeShopProvider = new CoffeeShopDataProvider();
+            var coffeeShopList = coffeeShopProvider.LoadCoffeeShops();
+
+            if(!coffeeShopList.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(coffeeShopList);
+        }
+    }
+}
